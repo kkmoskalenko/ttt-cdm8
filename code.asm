56a047768c6aa3139d1121436a6ab1e3a2232d44
LovelySharkPlush <37464348+LovelySharkPlush@users.noreply.github.com>
2020-05-26 22:38:50 +0700

    Add files via upload

--- code.asm
+++ code.asm
@@ -32,7 +32,7 @@ bnz lineHack
 inc r2
 ldi r1, 3
 ldi r3, table + 18
-jsr clRepeat
+jsr chooseLine + 4
 tst r0
 bnz cursedStrat
 
@@ -92,88 +92,47 @@ inc r2 #change symbol
 
 br readloop
 #
-
-gamestat: #overrites every reg
-		  #output in r0 (bits 6 and 7)
-	push r2
-	ldi r3, table #init loop
-	ldi r0, 9
-	gsloop:
-	if 
-		dec r0 #iterate
-	is z
-	then
-		pop r1 #if the number on top
-		if     #of the stack is 0 
-			tst r1 #then it's a draw
-		is nz
-			ldi r0, 0b11000000
-		fi
-		rts
-	fi	
-		
-	push r0
-	ldc r3, r0
-	ld r0, r0
-	inc r3
-	
-	ldc r3, r1 #r0, r1, r2 have the symbols
-	ld r1, r1  #of a single line
-	inc r3	
-	
-	ldc r3, r2
-	ld r2, r2
-	inc r3
-	if
-		tst r0
-	is z, or
-		tst r1
-	is z, or
-		tst r2
-	is z
-	then
-		pop r0 #if any one of them is 0
-		pop r2 #set the flag on stack to 0 and loop
-		clr r2
-		push r2
-		br gsloop
-	fi
-	
-	if
-		cmp r1, r2
-	is ne, or
-		cmp r0, r2
-	is ne
-	then
-		pop r0 #if any pair is ne - loop
-		br gsloop
-	fi
-	
-	pop r0 #preventing stack overflow
-	
-	#choosing the winner
-	if 
-		dec r1
-	is z
-		ldi r0, 0b10000000
-	else
-		ldi r0, 0b01000000
-	fi	
-	pop r1 #preventing stack overflow
-	
-	rts
-#
 	
 sendStuff:
 	st r1, r2 #store the symbol
 	
-	save r1 #save everything bc gamestat
-	save r2 #ovewrites all regs
-	save r3
-	jsr gamestat #checks for the game's end
-	restore 
-	restore
-	restore
+	push r1 
+	push r2 
+	push r3
+	
+	ldi r3, 16
+	ld r3, r1
+	inc r1
+	st r3, r1
+	clr r0
+	push r0
+	ldi r0, 4
+	cmp r1, r0
+	pop r0
+	ble clSkipEndCheck
+	
+	ldi r2, 0xf8
+	jsr chooseLine #checks for the game's end
+	if 
+		tst r0
+	is z 
+	then
+		
+		ldi r3, 16
+		ld r3, r1
+		push r0
+		ldi r0, 9
+		cmp r1, r0
+		pop r0
+		bls clSkipEndCheck
+		
+		ldi r0, 0b11000000
+	fi
+	
+	clSkipEndCheck:
+	pop r3
+	pop r2
+	pop r1
 	
 	#r1 has the cell addr bits
 	#r2 has the symbol bits
@@ -198,8 +157,8 @@ chooseLine: #dirty discipline
 			#places the "answer" to r0
 			#if r0 != 0, r3 will have the line
 			#where to place the symbol
-	ldi r3, table
 	ldi r1, 9
+	ldi r3, table
 	clRepeat:
 	while 
 		dec r1
