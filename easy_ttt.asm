c6cef8177167a00097f18cba944b17f0d7f223c3
LovelySharkPlush <37464348+LovelySharkPlush@users.noreply.github.com>
2020-05-27 13:46:02 +0700

    Hotfix code

--- easy_ttt.asm
+++ easy_ttt.asm
@@ -2,7 +2,7 @@ asect 0
 
 addsp -16
 
-ldi r2, 2
+ldi r2, 2 
 ldi r3, 0xf3
 readloop:
 ld r3, r1 #get the cell address
@@ -17,32 +17,24 @@ bnz readloop
 
 jsr sendStuff
 
-dec r2 #change symbol
-
-AIturn0:
-ldi r3, 0xf4 #(rng io addr)
-
 AIturn:
 
-st r3, r2
-ld r3, r1 #take rng and test it
+ldi r2, 0xf4
+ldi r3, table
+RNGloop:
 ldi r0, 8
+st r2, r2
+ld r2, r1	
+cmp r1, r0
+bgt RNGloop
+add r3, r1
+ldc r1, r1
+ld r1, r0
+tst r0
+bnz RNGloop 
 
-	cmp r0, r1
-bls AIturn
-
-ldi r3, table #get what's in the cell
-add r1, r3
-ldc r3, r1
-push r1
-ld r1, r1
-
-
-tst r1 #test the cell for emptiness
-pop r1	
-bnz AIturn0
-
-
+sendAI:
+ldi r2, 1
 ldi r3, 0xf3 #(ttt io addr)
 
 jsr sendStuff
@@ -51,88 +43,43 @@ inc r2 #change symbol
 
 br readloop
 #
-
-gamestat: #overrites every reg
-		  #output in r0 (bits 6 and 7)
-	push r2
-	ldi r3, table #init loop
-	ldi r0, 9
-	gsloop:
-	if 
-		dec r0 #iterate
-	is z
-	then
-		pop r1 #if the number on top
-		if     #of the stack is 0 
-			tst r1 #then it's a draw
-		is nz
-			ldi r0, 0b11000000
-		fi
-		rts
-	fi	
-		
-	push r0
-	ldc r3, r0
-	ld r0, r0
-	inc r3
-	
-	ldc r3, r1 #r0, r1, r2 have thee symbols
-	ld r1, r1  #of a single line
-	inc r3	
-	
-	ldc r3, r2
-	ld r2, r2
-	inc r3
-	if
-		tst r0
-	is z, or
-		tst r1
-	is z, or
-		tst r2
-	is z
-	then
-		pop r0 #if any one of them is 0
-		pop r2 #set the flag on stack to 0 and loop
-		clr r2
-		push r2
-		br gsloop
-	fi
-	
-	if
-		cmp r1, r2
-	is ne, or
-		cmp r0, r2
-	is ne
-	then
-		pop r0 #if any pair is ne - loop
-		br gsloop
-	fi
-	
-	pop r0 #preventing stack overflow
-	
-	#choosing the winner
-	if 
-		dec r1
-	is z
-		ldi r0, 0b01000000
-	else
-		ldi r0, 0b10000000
-	fi	
-	pop r1 #preventing stack overflow
-	
-	rts
-#
 	
 sendStuff:
 	st r1, r2 #store the symbol
 	
-	save r1 #save everything bc gamestat
-	save r2 #ovewrites all regs
-	save r3
-	jsr gamestat #checks for the game's end
-	restore 
-	restore
-	restore
+	push r1 
+	push r2 
+	push r3
+	
+	ldi r3, 16 #at addr 0x10 is a count of turns
+	ld r3, r1
+	inc r1
+	st r3, r1
+	clr r0
+	ldi r3, 4
+	cmp r1, r3
+	ble clSkipEndCheck
+	
+	ldi r2, 0xf8
+	jsr chooseLine #checks for the game's end
+	if 
+		tst r0
+	is z 
+	then
+		
+		ldi r3, 16
+		ld r3, r1
+		ldi r3, 9
+		cmp r3, r1
+		bgt clSkipEndCheck
+		
+		ldi r0, 0b11000000
+	fi
+	
+	clSkipEndCheck:
+	pop r3
+	pop r2
+	pop r1
 	
 	#r1 has the cell addr bits
 	#r2 has the symbol bits
@@ -152,6 +99,59 @@ sendStuff:
 
 	rts
 #
+
+chooseLine: #dirty discipline
+			#places the "answer" to r0
+			#if r0 != 0, r3 will have the line
+			#where to place the symbol
+	ldi r1, 9
+	ldi r3, table
+	clRepeat:
+	while 
+		dec r1
+	stays nz
+		
+		push r1
+		jsr clThing
+		jsr clThing
+		jsr clThing
+		st r2, r1
+		pop r1
+		
+		
+		ld r2, r0
+		if
+			tst r0
+		is nz
+		then
+			dec r3
+			dec r3
+			dec r3
+			rts 
+		fi
+	wend
+	rts	
+
+AItableThing:
+	inc r3 
+	inc r3
+	ldc r3, r1
+	ld r1, r0
+	tst r0
+	pop r0
+	bz sendAI
+	push r0
+	rts
+	
+clThing:
+	ldc r3, r0
+	ld r0, r0
+	shla r1
+	shla r1
+	or r0, r1
+	inc r3
+	rts
+	
 
 table: 
 	dc 0,1,2 #h
